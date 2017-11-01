f52b5bca0190f609b88153d42275b0d26aca0169
hnkisadiq <hnkisadiq@gmail.com>
2017-11-01 23:42:56 +0530

    Update views.py

--- ITS/client/views.py
+++ ITS/client/views.py
@@ -18,11 +18,14 @@ def test(request):
     url1 = 'http://10.0.3.23:8076/farm/'
     url2 = 'http://10.0.3.23:8076/house/'
     url3 = 'http://10.0.3.23:8076/well/'
+    url4 = 'http://10.0.3.23:8076/well/'
     response1 = urllib.urlopen(url1)
     response2 = urllib.urlopen(url2)
     response3 = urllib.urlopen(url3)
+    response4 = urllib.urlopen(url4)
     js_data1 = json.loads(response1.read())
     js_data2 = json.loads(response2.read())
     js_data3 = json.loads(response3.read())
-    return render(request,'index.html', {"farm": js_data1,"house": js_data2,"well":js_data3})
+    js_data4 = json.loads(response4.read())
+    return render(request,'index.html', {"farm": js_data1,"house": js_data2,"well":js_data3,"crop":js_data4})
 
