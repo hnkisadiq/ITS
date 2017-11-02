ecc919de4577714ec84507cdf3aea902f5c12d24
hnkisadiq <hnkisadiq@gmail.com>
2017-11-02 11:42:57 +0530

    Update views.py

--- ITS/client/views.py
+++ ITS/client/views.py
@@ -75,7 +75,7 @@ def fmail(request):
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
         return redirect('thanks')
-    return render(request, "email.html")
+    return render(request, "index.html")
 
 def thanks(request):
     return HttpResponse('Thank you for your message.')
